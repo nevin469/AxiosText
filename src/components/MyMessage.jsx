fb64caef858c3bbb7316a8855052ccfac8999523
Nevin Bullywon <nbullywo@uwaterloo.ca>
2021-12-29 22:06:12 -0500

    Changed message color

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -11,7 +11,7 @@ const MyMessage = ({ message }) => {
     }
   
     return (
-      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+      <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#1c3dd1' }}>
         {message.text}
       </div>
     );
